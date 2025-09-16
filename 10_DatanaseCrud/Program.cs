13ea05f32652cdf6e01de12a0a35b99ec78c77a7
Arda Mantar <ardamantar0@gmail.com>
2025-09-16 10:17:11 +0300

    Added Database partition

---
+++ 10_DatanaseCrud/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatanaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crud--> Create-Read-Update-Delete
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek İstediğiniz Kategori Adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source= DESKTOP-5D0LQCC; initial catalog = EgitimKampiDb; integrated security = true");
+            //connection.Open();  
+            //SqlCommand command = new SqlCommand("INSERT INTO TblCategory (CategoryName) values (@p1)",connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Kategori Ekleme İşlemi Başarılı");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+            //string productName;
+            //decimal productPrice;
+            ////bool productStatus;
+            //Console.Write("Ürün Adı: ");
+            //productName = Console.ReadLine();
+
+            //Console.Write("Ürün Fiyatı: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data source = DESKTOP-5D0LQCC;initial catalog = EgitimKampiDb; integrated security = true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Insert into TblProduct (ProductName,ProductPrice,ProductStatus) values (@ProductName,@ProductPrice,@ProductStatus)", connection);
+            //command.Parameters.AddWithValue("@ProductName", productName);
+            //command.Parameters.AddWithValue("@ProductPrice", productPrice);
+            //command.Parameters.AddWithValue("@ProductStatus", true);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Ürün eklemesi başarılı");
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+            //SqlConnection connection = new SqlConnection("Data source = DESKTOP-5D0LQCC;initial catalog = EgitimKampiDb; integrated security = true");
+
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * from TblProduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach(DataRow  row in dataTable.Rows) // Her bir satırı alır
+            //{
+            //    foreach(var item in row.ItemArray) // Her satırdaki hücreleri alır
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //connection.Close();
+
+
+            #endregion
+
+            #region Ürün Silme İşlemi
+            //Console.Write("Silinecek Ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data source = DESKTOP-5D0LQCC;initial catalog = EgitimKampiDb; integrated security = true");
+
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Delete From TblProduct where ProductId = @productId",connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Silme İşlemi başarılı");
+
+
+
+
+            #endregion
+
+            #region Ürün Güncelleme
+
+            Console.Write("Güncellenecek Ürün Id: ");
+            int productId = int.Parse(Console.ReadLine());
+
+            Console.Write("Güncellenecek Ürün Adı: ");
+            string productName =Console.ReadLine();
+
+            Console.Write("Güncellenecek Ürün Fiyatı: ");
+            decimal productPrice = decimal.Parse(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection("Data source = DESKTOP-5D0LQCC;initial catalog = EgitimKampiDb; integrated security = true");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Update TblProduct Set ProductName = @productName , ProductPrice = @productPrice where ProductId = @productId",connection);
+            command.Parameters.AddWithValue("@productName",productName);
+            command.Parameters.AddWithValue("@productPrice", productPrice);
+            command.Parameters.AddWithValue("@productId", productId);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            Console.WriteLine("Güncelleme İşlemi Başarılı");
+
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 10_DatanaseCrud/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("10_DatanaseCrud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10_DatanaseCrud")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd5fd71f-204f-4e93-ac34-88f5a61554bc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
